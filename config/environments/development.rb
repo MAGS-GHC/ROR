53ca35fffec1d00b9515f159de1cf2f24278b8c9
MAGS <103378236+MathiasGScom@users.noreply.github.com>
2023-10-16 21:09:30 +0200

    Added Hostname

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -68,3 +68,6 @@ Rails.application.configure do
   # Uncomment if you wish to allow Action Cable access from any origin.
   # config.action_cable.disable_request_forgery_protection = true
 end
+Rails.application.configure do
+  config.hosts << "https://rails-production-a07d.up.railway.app/"
+end
