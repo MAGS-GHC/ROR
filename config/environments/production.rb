47918e4a51dfb5dfda4865143fd39f617ce35ca4
MAGS <103378236+MathiasGScom@users.noreply.github.com>
2023-10-16 21:22:50 +0200

    Added host to prod

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -91,3 +91,6 @@ Rails.application.configure do
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
 end
+Rails.application.configure do
+  config.hosts << "rails-production-a07d.up.railway.app"
+end
